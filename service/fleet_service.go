d0520a4531c887cb91b9c5116c30c7620441492f
Mafatichul Fuadi <muhammad_nm_fua_x@telkomsel.co.id>
2025-12-27 23:46:12 +0700

    Detail Fleet | Add new API to fetch detail fleet

--- service/fleet_service.go
+++ service/fleet_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"service-travego/configs"
@@ -36,6 +37,7 @@ func (s *FleetService) CreateFleet(createdBy, organizationID string, req *model.
 func (s *FleetService) GetServiceFleets() ([]model.ServiceFleetItem, error) {
 	items, err := s.repo.GetServiceFleets()
 	if err != nil {
+		fmt.Println("Error fetching service fleets:", err)
 		return nil, err
 	}
 
@@ -56,6 +58,79 @@ func (s *FleetService) GetServiceFleets() ([]model.ServiceFleetItem, error) {
 		}
 	}
 	return items, nil
+}
+
+func (s *FleetService) GetServiceFleetDetail(fleetID string) (*model.ServiceFleetDetailResponse, error) {
+	// First resolve OrgID
+	orgID, err := s.repo.GetFleetOrgID(fleetID)
+	if err != nil {
+		return nil, NewServiceError(ErrNotFound, http.StatusNotFound, "fleet not found")
+	}
+
+	meta, err := s.repo.GetFleetDetailMeta(orgID, fleetID)
+	if err != nil {
+		return nil, NewServiceError(ErrNotFound, http.StatusNotFound, "fleet not found")
+	}
+	fac, err := s.repo.GetFleetFacilities(fleetID)
+	if err != nil {
+		fac = []string{}
+	}
+	pickup, err := s.repo.GetFleetPickup(orgID, fleetID)
+	if err != nil {
+		pickup = []model.FleetPickupItem{}
+	}
+	addon, err := s.repo.GetFleetAddon(orgID, fleetID)
+	if err != nil {
+		addon = []model.FleetAddonItem{}
+	}
+	prices, err := s.repo.GetFleetPrices(orgID, fleetID)
+	if err != nil {
+		prices = []model.FleetPriceItem{}
+	}
+	images, err := s.repo.GetFleetImages(fleetID)
+	if err != nil {
+		images = []model.FleetImageItem{}
+	}
+
+	s.ensureCitiesLoaded()
+
+	// Convert Pickup
+	svcPickup := make([]model.ServiceFleetPickupItem, len(pickup))
+	for i, p := range pickup {
+		svcPickup[i] = model.ServiceFleetPickupItem{
+			CityID: p.CityID,
+		}
+		key := intToString(p.CityID)
+		if name, ok := s.citiesName[key]; ok {
+			svcPickup[i].CityName = name
+		} else {
+			svcPickup[i].CityName = ""
+		}
+	}
+
+	// Convert Pricing
+	svcPrices := make([]model.ServiceFleetPriceItem, len(prices))
+	for i, p := range prices {
+		svcPrices[i] = model.ServiceFleetPriceItem{
+			Duration:      p.Duration,
+			RentType:      p.RentType,
+			RentTypeLabel: configs.RentType(p.RentType).String(),
+			Price:         p.Price,
+			DiscAmount:    p.DiscAmount,
+			DiscPrice:     p.DiscPrice,
+			Uom:           p.Uom,
+		}
+	}
+
+	resp := &model.ServiceFleetDetailResponse{
+		Meta:       *meta,
+		Facilities: fac,
+		Pickup:     svcPickup,
+		Addon:      addon,
+		Pricing:    svcPrices,
+		Images:     images,
+	}
+	return resp, nil
 }
 
 func (s *FleetService) ListFleets(req *model.ListFleetRequest) ([]model.FleetListItem, error) {
